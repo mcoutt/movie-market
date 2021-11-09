76054baa5b15ed525f061f236fa3c8aaa45d847c
Mykhailo <Mykhailo_Koshelnyk@epam.com>
2021-11-09 19:16:34 +0200

    Add 404 and "Not found" tempaltes

---
+++ src/components/pages/404.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Page404 = () => {
+  <div>
+    <p>404</p>
+    <a href="/">Back to Home page</a>
+  </div>;
+};
+
+export default Page404;
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,1 +1,3 @@
 export * from "./home-page";
+export * from "./not-found";
+export * from "./404";
